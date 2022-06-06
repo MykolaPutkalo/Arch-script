0d38a26026e0c5f6c765833a3d81f074afcf51c3
MykolaPutkalo <106990614+MykolaPutkalo@users.noreply.github.com>
2022-06-06 19:41:06 +0300

    Add files via upload

---
+++ kde.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c Kiev -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
+sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescreenrecorder obs-studio vlc papirus-icon-theme kdenlive materia-kde
+
+sudo systemctl enable sddm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+sudo reboot
