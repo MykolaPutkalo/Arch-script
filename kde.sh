0a7a48b71a86ba614cf0c9b86d7ebe28d3004cec
MykolaPutkalo <106990614+MykolaPutkalo@users.noreply.github.com>
2022-06-06 21:01:07 +0300

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -10,7 +10,7 @@ sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
 
-sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescreenrecorder obs-studio vlc papirus-icon-theme kdenlive materia-kde
+sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescreenrecorder papirus-icon-theme kdenlive materia-kde
 
 sudo systemctl enable sddm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
